796ac9f1bf87aa41637808909e836acab9fa7716
S11cky <matejsykora345@gmail.com>
2025-09-19 17:46:30 +0200

    fix(ci): remove backup from src and fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({`r`n  base: "/simple-token-dapp/",
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/simple-token-dapp/",
   plugins: [react()],
-})
+});
